054206ed3f6f39cb57ea146d3bfebdf80a640a93
acorbat <acorbat@df.uba.ar>
2018-06-06 11:01:16 -0300

    Documented function to find foci.

--- foci_finder/foci_analysis.py
+++ foci_finder/foci_analysis.py
@@ -3,10 +3,10 @@ from scipy.ndimage import gaussian_laplace
 from skimage.measure import label
 
 def find_foci(stack):
-    """Receives a single 3D stack of images and returns a same size lebelled image with all the foci."""
-    LoG_filtered = gaussian_laplace(stack, [2, 2, 2], mode='nearest')
-    LoG_classif = KMeans(n_clusters=2).fit_predict(LoG_filtered.flatten().reshape(1, -1).T)
-    LoG_classif = LoG_classif.reshape(LoG_filtered.shape)
-    labeled = label(LoG_classif)
+    """Receives a single 3D stack of images and returns a same size labeled image with all the foci."""
+    filtered = gaussian_laplace(stack, [2, 2, 2], mode='nearest')  # Filter image with LoG (correlates with blobs)
+    classif = KMeans(n_clusters=2).fit_predict(filtered.flatten().reshape(1, -1).T)  # all pixels are handled as list
+    classif = classif.reshape(filtered.shape)  # reshaping classification
+    labeled = label(classif)  # labelling in 3D
 
     return labeled
