c4c9cb143be174c92ddfda61c638a2d3e0c8a388
acorbat <acorbat@df.uba.ar>
2018-06-08 19:25:05 -0300

    Modified foci_finder to implement my_KMeans.

--- foci_finder/foci_analysis.py
+++ foci_finder/foci_analysis.py
@@ -19,8 +19,7 @@ def my_KMeans(stack, clusters=2):
 def find_foci(stack):
     """Receives a single 3D stack of images and returns a same size labeled image with all the foci."""
     filtered = gaussian_laplace(stack, [2, 2, 2], mode='nearest')  # Filter image with LoG (correlates with blobs)
-    classif = KMeans(n_clusters=2).fit_predict(filtered.flatten().reshape(1, -1).T)  # all pixels are handled as list
-    classif = classif.reshape(filtered.shape)  # reshaping classification
+    classif = my_KMeans(filtered)  # all pixels are handled as list
     classif = binary_opening(classif)  # maybe it's unnecessary or a hyper parameter
     labeled = label(classif)  # labelling in 3D
 
