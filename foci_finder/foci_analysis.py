0dbffc8a25e6148121343afc939cfe9f753e02bc
acorbat <acorbat@df.uba.ar>
2018-06-08 18:39:42 -0300

    Added K Means function to take stack and ignore Nans.

--- foci_finder/foci_analysis.py
+++ foci_finder/foci_analysis.py
@@ -1,7 +1,20 @@
+import numpy as np
+
 from sklearn.cluster import KMeans
 from scipy.ndimage import gaussian_laplace
 from skimage.measure import label
 from skimage.morphology import binary_opening
+
+
+def my_KMeans(stack, clusters=2):
+    """Applies K Means algorithm to a whole stack ignoring NaNs and returns a stack of the same shape with the
+    classification"""
+    vals = stack.flatten().reshape(1, -1).T
+    real_inds = np.isfinite(vals)
+    classif = KMeans(n_clusters=clusters).fit_predict(vals[real_inds][:, np.newaxis])
+    vals[real_inds] = classif
+    return vals.reshape(stack.shape)
+
 
 def find_foci(stack):
     """Receives a single 3D stack of images and returns a same size labeled image with all the foci."""
