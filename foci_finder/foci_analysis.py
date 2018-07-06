259fb2842b66ab83161057123cc04e314b32952f
acorbat <acorbat@df.uba.ar>
2018-07-06 15:58:23 -0300

    Added parameter to avoid necesity of specifying cell mask when image is zoomed into cell.

--- foci_finder/foci_analysis.py
+++ foci_finder/foci_analysis.py
@@ -116,14 +116,15 @@ def find_mito(stack, cell_mask, foci_mask):
 
 
 def segment_all(foci_stack, mito_stack, subcellular=False):
-    """Takes foci and mitochondrial stacks and returns their segmentations. If mito_stack is None, mito_segm is None."""
+    """Takes foci and mitochondrial stacks and returns their segmentations. If mito_stack is None, mito_segm is None. If
+    subcellular is True then cell_segm is all ones as you should be zoomed into the citoplasm."""
     foci_labeled = find_foci(foci_stack)
 
     if subcellular:
         cell_segm = np.ones_like(foci_stack)
     else:
         cell_segm = find_cell(foci_stack, foci_labeled > 0)
-    
+
     if mito_stack is not None:
         mito_segm = find_mito(mito_stack, cell_segm, foci_labeled > 0)
     else:
